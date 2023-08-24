df804174dbef9284f05f2bc809679431ccd4e702
Semuel <serg_artemenko@ukr.net>
2023-08-24 17:46:58 +0300

    feat: created App.module.css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Header, Main, About, Footer } from './components'
 
+import styles from './App.module.css'
 
 function App() {
   return (
-    <div>
+    <div className={styles.main}>
         <BrowserRouter>
           <Header />
-          <Routes> 
-            <Route exact path='/' element={<Main />} />
-            <Route path='/about' element={<About />} />
-            
-          </Routes>
+          <div className={styles.pageContent}>
+            <Routes> 
+              <Route exact path='/' element={<Main />} />
+              <Route path='/about' element={<About />} />
+            </Routes>
+          </div>
           <Footer />
         </BrowserRouter>
     </div>
