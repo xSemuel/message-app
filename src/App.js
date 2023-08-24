9ee0bdd9dba84e3013790b62e01613f3bcf16c09
Semuel <serg_artemenko@ukr.net>
2023-08-24 17:10:47 +0300

    feat: added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { BrowserRouter } from "react-router-dom";
+import { Header } from './components'
+
+
 function App() {
   return (
     <div>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
     </div>
   );
 }
