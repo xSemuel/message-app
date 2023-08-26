ad95d636d1ce174c6687926d0b468294a81a0493
Semuel <serg_artemenko@ukr.net>
2023-08-27 00:37:42 +0300

    refactor: splitting the header component into 2 components

--- src/components/header/Burgermenu.js
+++ src/components/header/Burgermenu.js
@@ -1,9 +1,37 @@
-const Burgermenu = () => {
+import { useState } from 'react';
+import { NavLink } from "react-router-dom";
+import { NAVIGATION_LINKS } from '../../constants'
+import menuAdaptive from './../../image/menuAdaptive.svg';
+import styles from './Header.module.css';
+
+export function Burgermenu() {
+
+    const [navbarOpen, setNavbarOpen] = useState(false);
+
+    function viewMobileMenu(event) {
+        event.preventDefault();
+        setNavbarOpen((prev) => !prev);
+        console.log(navbarOpen)
+    }
+
+    function closeMenuMobile() {
+        setNavbarOpen(false);
+    }
+
+    const namAdaptiveMenu = NAVIGATION_LINKS.map(({ text, link }, index) => 
+    <li key={index}><NavLink className={styles.menuItemMobile} to={link} onClick={closeMenuMobile}>{text}</NavLink></li>
+    );
+
     return ( 
         <div>
-            
+            <button className={styles.mobileMenuButton} onClick={viewMobileMenu}>
+                <img src={menuAdaptive} alt="Мобільна навігація" />
+                {/* {navbarOpen ? 'close' : 'open'} */}
+            </button> 
+            <ul className={styles.mobileMenuToogle}>
+                {navbarOpen && namAdaptiveMenu}
+            </ul>
         </div>
      );
 }
  
-export default Burgermenu;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,29 +1,11 @@
 import { NavLink } from "react-router-dom";
-import { useState } from 'react';
 import logoIcon from '../../image/MessageApp_logo.svg';
-import menuAdaptive from './../../image/menuAdaptive.svg';
-import { NAVIGATION_LINKS } from '../../constants'
+import { NAVIGATION_LINKS } from '../../constants';
+import { Burgermenu } from "./Burgermenu";
 import styles from './Header.module.css';
 
 
 export function Header() {
-
-    const [navbarOpen, setNavbarOpen] = useState(false);
-
-    function viewMobileMenu(event) {
-        event.preventDefault();
-        setNavbarOpen((prev) => !prev);
-        console.log(navbarOpen)
-    }
-
-    function closeMenuMobile() {
-        setNavbarOpen(false);
-    }
-
-    const namAdaptiveMenu = NAVIGATION_LINKS.map(({ text, link }, index) => 
-    <li key={index}><NavLink className={styles.menuItemMobile} to={link} onClick={closeMenuMobile}>{text}</NavLink></li>
-    );
-
 
     return (
         <div className={styles.headerWrapper}>
@@ -38,15 +20,8 @@ export function Header() {
                         )}
                     </ul>
                 </nav>  
-                <button className={styles.mobileMenuButton} onClick={viewMobileMenu}>
-                    <img src={menuAdaptive} alt="Мобільна навігація" />
-                    {/* {navbarOpen ? 'close' : 'open'} */}
-                </button> 
-                <ul className={styles.mobileMenuToogle}>
-                    {navbarOpen && namAdaptiveMenu}
-                </ul>
-            </div>
-           
+                <Burgermenu />    
+            </div>                
         </div>
     );
 }
