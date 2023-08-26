24b5932725a0cc73094a5bf385b50ee1fb0c906a
Semuel <serg_artemenko@ukr.net>
2023-08-27 02:27:56 +0300

    feat: added hook useOnClickOutside in Burgermenu component

--- src/components/header/Burgermenu.js
+++ src/components/header/Burgermenu.js
@@ -1,11 +1,13 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { NavLink } from "react-router-dom";
+import { useOnClickOutside } from 'usehooks-ts'
 import { NAVIGATION_LINKS } from '../../constants'
 import menuAdaptive from './../../image/menuAdaptive.svg';
 import styles from './Header.module.css';
 
 export function Burgermenu() {
 
+    const ref = useRef(null)
     const [navbarOpen, setNavbarOpen] = useState(false);
 
     function viewBurgerMenu(event) {
@@ -18,6 +20,9 @@ export function Burgermenu() {
         setNavbarOpen(false);
     }
 
+    useOnClickOutside(ref, closeBurgerMenu);
+
+
     const navAdaptiveMenu = NAVIGATION_LINKS.map(({ text, link }, index) => 
     <li key={index}><NavLink className={styles.burgerMenuItem} to={link} onClick={closeBurgerMenu}>{text}</NavLink></li>
     );
@@ -28,7 +33,7 @@ export function Burgermenu() {
                 <img src={menuAdaptive} alt="Мобільна навігація" />
                 {/* {navbarOpen ? 'close' : 'open'} */}
             </button> 
-            { navbarOpen && <ul className={styles.burgerMenuToogle}>
+            { navbarOpen && <ul className={styles.burgerMenuToogle} ref={ref}>
                 {navAdaptiveMenu}
             </ul>}
         </>
