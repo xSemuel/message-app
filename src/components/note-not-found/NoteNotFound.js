f5a332c335426842d1eb8db84cd9354aab6981d8
Semuel <serg_artemenko@ukr.net>
2023-08-27 17:05:24 +0300

    feat: added NoteNotFound.module.css file

--- src/components/note-not-found/NoteNotFound.js
+++ src/components/note-not-found/NoteNotFound.js
@@ -1,11 +1,12 @@
 import notFound from '../../image/404NotFound.png';
+import styles from './NoteNotFound.module.css';
 
 export function NoteNotFound() {
     return (
-        <div>
+        <div className={styles.notFoundContainer}>
             <h3> Помилка 404. Сторінка на яку Ви посилаєтесь не знайдено!</h3>
             <div>
-                <img src={notFound} alt="Сторінка не знайдена" />
+                <img className={styles.imgNotFound} src={notFound} alt="Сторінка не знайдена" />
             </div>
         </div>
     );
